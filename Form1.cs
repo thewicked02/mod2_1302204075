5388855c0202b56d62f8f6dc5d1d2aae2fd16ef5
rodo <rodonatanael02@gmail.com>
2022-03-05 16:50:18 +0700

    menambahkan implementasi kalkulator

--- Form1.cs
+++ Form1.cs
@@ -7,69 +7,75 @@ namespace modul2_1302204075
             InitializeComponent();
         }
 
+        int hitung = 0;
+        int num1 = 0;
+       
+
         private void label1_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            btn1.Text = "1";
+            textBox1.Text = textBox1.Text + 1;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = textBox1.Text + 2;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = textBox1.Text + 3;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = textBox1.Text + 4;
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = textBox1.Text + 5;
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = textBox1.Text + 6;
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = textBox1.Text + 7;
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = textBox1.Text + 8;
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = textBox1.Text + 9;
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
-
+            hitung = 1;
+            num1 = int.Parse(textBox1.Text);
+            textBox1.Text = "";
         }
 
         private void button11_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = textBox1.Text + 0;
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = (num1 + int.Parse(textBox1.Text)).ToString();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
